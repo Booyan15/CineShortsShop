c3f1b7cf37cb0b68e662a656babee213a4175050
Bojan Popovski <144334411+Booyan15@users.noreply.github.com>
2024-10-01 22:09:25 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -4,3 +4,29 @@ function toggleMenu() {
     const navbar = document.getElementById('navbar');
     navbar.classList.toggle('show'); // Toggle the 'show' class to display/hide the menu
 }
+
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Select all anchor links with hashes
+    const smoothScrollLinks = document.querySelectorAll('a[href*="#"]');
+
+    smoothScrollLinks.forEach(link => {
+        link.addEventListener('click', function (e) {
+            // Prevent default anchor click behavior
+            e.preventDefault();
+
+            // Store hash
+            const hash = this.hash;
+
+            // Using window.scrollTo to animate scroll
+            if (hash) {
+                // Scroll to the specific section
+                window.scrollTo({
+                    top: document.querySelector(hash).offsetTop,
+                    behavior: 'smooth'
+                });
+            }
+        });
+    });
+});
