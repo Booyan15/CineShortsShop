e0c414234b62210bc970a569043ef63a9699dd41
Bojan Popovski <144334411+Booyan15@users.noreply.github.com>
2024-10-01 21:54:20 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+
+function toggleMenu() {
+    const navbar = document.getElementById('navbar');
+    navbar.classList.toggle('show'); // Toggle the 'show' class to display/hide the menu
+}
